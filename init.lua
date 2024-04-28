43a7434ecad4b15e5d006ac25780fcdec658fb76
Baptiste Blanchard <baptiste@blanchard.xyz>
2024-04-28 11:10:32 +0200

    adding ruff_lsp for my python files

--- init.lua
+++ init.lua
@@ -541,7 +541,7 @@ require('lazy').setup({
         -- clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        ruff_lsp = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
